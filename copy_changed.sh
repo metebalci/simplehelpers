9589d3ec61ff86f7ba611984b4ab5d871ad2b7a1
Mete Balci <metebalci@gmail.com>
2019-03-03 10:55:57 +0100

    ignore errors

--- copy_changed.sh
+++ copy_changed.sh
@@ -2,4 +2,4 @@
 # this can be run in root's crontab
 # sudo is needed because some files can be protected
 
-debsums -ce | while read line ; do cp --parents $line <WRITE_HERE_TARGET_DIRECTORY> ; done
+debsums -ce --ignore-permissions --ignore-obsolete | while read line ; do cp -v --parents $line <WRITE_HERE_TARGET_DIRECTORY> ; done
